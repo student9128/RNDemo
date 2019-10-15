72b4d2720877cf1ff697caa488661916066f72dc
student9128 <jingpengchn@163.com>
2019-10-15 22:15:14 +0800

    add:react native和原生交互的一些方法,原生发送事件到react native.

--- android/app/src/main/java/com/rndemo/RNModule.java
+++ android/app/src/main/java/com/rndemo/RNModule.java
@@ -1,14 +1,20 @@
 package com.rndemo;
 
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,8 +26,11 @@ import java.util.Map;
 public class RNModule extends ReactContextBaseJavaModule {
     private static final String DURAION_SHORT_KEY = "SHORT";
     private static final String DURAION_LONG_KEY = "LONG";
+    private ReactContext reactContext;
+
     public RNModule(@NonNull ReactApplicationContext reactContext) {
         super(reactContext);
+        this.reactContext = reactContext;
     }
 
     @NonNull
@@ -40,15 +49,37 @@ public class RNModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void showToast(String msg,int duration) {
-        Toast.makeText(getReactApplicationContext(), msg,duration).show();
+    public void showToast(String msg, int duration) {
+        Toast.makeText(getReactApplicationContext(), msg, duration).show();
     }
+
     @ReactMethod
-    public void showBoolean(String msg, Callback callback){
-        if (msg.equals("1")||msg.equals("true")){
+    public void showBoolean(String msg, Callback callback) {
+        if (msg.equals("1") || msg.equals("true")) {
             callback.invoke(true);
-        }else {
+        } else {
             callback.invoke(false);
         }
+    }
+
+    @ReactMethod
+    public void usePromise(String msg, Promise promise) {
+        if (TextUtils.isEmpty(msg)) {
+            WritableMap map = Arguments.createMap();
+            map.putString("content", "收到的消息为空");
+            promise.resolve(map);
+        } else {
+            WritableMap map = Arguments.createMap();
+            map.putString("content", "收到的消息为：" + msg);
+            promise.resolve(map);
+        }
+
+    }
+    @ReactMethod
+    public void sendEvent(){
+        WritableMap map = Arguments.createMap();
+        map.putString("content","消息来自Native");
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit("toRN",map);
     }
 }
--- src/reactNativeInteractionScreen.js
+++ src/reactNativeInteractionScreen.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import { View, 
+import {
+  View,
   Text,
   Button,
   TouchableNativeFeedback,
   TextInput,
-  NativeModules } from 'react-native';
+  NativeModules,
+  StyleSheet,
+  NativeEventEmitter,
+  DeviceEventEmitter
+} from 'react-native';
 const RNModule = NativeModules.RNModule
 import TextButton from './common/TextButton';
 import Colors from './common/colors'
-import { deviceWidth,realDeviceHeight } from './common/Utils';
+import { deviceWidth, realDeviceHeight } from './common/Utils';
 /**
  * RN与原生交互
  */
@@ -16,52 +21,88 @@ class ReactNativeInteractionScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      textValue:''
+      textValue: ''
     };
   }
+  async usePromise() {
+    try {
+      const { content } = await RNModule.usePromise(this.state.textValue+'第二个')
+      alert(content)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+componentDidMount(){
+  const eventEmitter = new NativeEventEmitter(RNModule)
+  eventEmitter.addListener('toRN',(event)=>{
+    alert(event.content)
+  })
 
+}
   render() {
     return (
       <View>
         <Text> reactNativeInteractionScreen </Text>
         <TouchableNativeFeedback
-        onPress={()=>{RNModule.showToast('abs',RNModule.SHORT)}}>
-            <View style={{backgroundColor:'red',alignSelf:'flex-start',
-            paddingHorizontal:10,
-            paddingVertical:10,
-            borderRadius:5}}>
+          onPress={() => { RNModule.showToast('abs', RNModule.SHORT) }}>
+          <View style={{
+            backgroundColor: 'red', alignSelf: 'flex-start',
+            paddingHorizontal: 10,
+            paddingVertical: 10,
+            borderRadius: 5
+          }}>
             <Text>showToast</Text>
-            </View>
+          </View>
         </TouchableNativeFeedback>
         <TextInput
-         autoCapitalize='none'
-         autoFocus={false}
-         multiline={false}
-         style={{borderColor:Colors.colorPrimary,
-          borderWidth:2,
-          paddingHorizontal:5,
-          paddingVertical:2,
-          borderRadius:5,
-          width:deviceWidth()-30
-        }}
-         underlineColorAndroid="transparent"
-        maxLength={18}
-        onChangeText={(text)=>this.state.textValue=text}/>
+          autoCapitalize='none'
+          autoFocus={false}
+          multiline={false}
+          style={styles.textInputStyle}
+          underlineColorAndroid="transparent"
+          maxLength={18}
+          onChangeText={(text) => this.state.textValue = text} />
         <TextButton title='通过callback和原生交互'
-        onPress={()=>{
-          RNModule.showBoolean(this.state.textValue,(callback)=>{
-            if(callback){
-              RNModule.showToast('true',RNModule.SHORT)
-            }else{
-              RNModule.showToast('false',RNModule.SHORT)
+          onPress={() => {
+            RNModule.showBoolean(this.state.textValue, (callback) => {
+              if (callback) {
+                RNModule.showToast('true', RNModule.SHORT)
+              } else {
+                RNModule.showToast('false', RNModule.SHORT)
 
-            }
-          })
-        }}
-    />
+              }
+            })
+          }}
+        />
+        <TextButton title='通过Promise和原生交互'
+          onPress={() => {
+            RNModule.usePromise(this.state.textValue)
+              .then((x) => {
+                alert(x.content)
+              })
+          }} />
+        <TextButton title='通过Promise和原生交互'
+          onPress={() => {
+            this.usePromise()
+          }} />
+        <TextButton title='发送事件到RN'
+          onPress={() => {
+            RNModule.sendEvent()
+          }} />
       </View>
     );
   }
 }
 
 export default ReactNativeInteractionScreen;
+
+const styles = StyleSheet.create({
+  textInputStyle: {
+    borderColor: Colors.colorPrimary,
+    borderWidth: 2,
+    paddingHorizontal: 5,
+    paddingVertical: 2,
+    borderRadius: 5,
+    width: deviceWidth() - 30
+  }
+})
