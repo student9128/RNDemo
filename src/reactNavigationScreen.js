e148ff05e211013f19d2eded3b52f433cd87178e
student9128 <jingpengchn@163.com>
2019-10-24 10:26:11 +0800

    add:添加react-navigation-tabs依赖,使用底部导航栏

--- src/reactNavigationScreen.js
+++ src/reactNavigationScreen.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text,Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
+import {  } from 'react-navigation';
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
 class ReactNavigationScreen extends Component {
   constructor(props) {
     super(props);
@@ -17,5 +39,15 @@ class ReactNavigationScreen extends Component {
     );
   }
 }
+const TabNavigator = createBottomTabNavigator({
+  Home:HomeScreen,
+  SettingsScreen:SettingsScreen
+},{
+  defaultNavigationOptions:({navigation})=>({
 
-export default ReactNavigationScreen;
+  })
+})
+
+// export default ReactNavigationScreen;
+// export default createAppContainer(TabNavigator);
+export default TabNavigator;
